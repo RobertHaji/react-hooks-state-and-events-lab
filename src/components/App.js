7d7c745fffb10a0ed9a702261ffb1f4eb1d0272f
Robert Haji <roberthadji66@gmail.com>
2025-04-29 10:03:46 +0300

    done-react-hook-state

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,27 @@
-import React from "react";
+
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import React, { useState } from "react";
+
 
 function App() {
+  const [isLight, setIsLight] = useState(false);
+
+
+  const handleDoubleClick = () => {
+    setIsLight(!isLight);
+  };
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
-
+  // const appClass = false ? "App dark" : "App light";
+const appClass = isLight ? "App dark" : "App light"
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleDoubleClick}>{isLight ? "Light Mode" : "Dark Mode"}</button>
+        
       </header>
       <ShoppingList items={itemData} />
     </div>
